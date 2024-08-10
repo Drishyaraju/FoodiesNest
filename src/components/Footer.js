ded4ba59c3824ec3e4aa0dc536a19112f01a31d5
Drishyaraju <drishyaraju13@gmail.com>
2024-08-10 23:14:52 +0530

    update

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return(
+        <div className="footer container">
+        <div className="footer-section">
+            <p className="title">FoodiesNest.com</p>
+            <p>FoodiesNest is a place where we believe in feeding both your soul and your appetite. Enjoy a treasure trove of free recipes. Dive in now!</p>
+            <p>&copy; 2024 | All Rights Reserved</p>
+        </div>
+        <div className="footer-section">
+            <p className="title">Contact Us</p>
+            <p>foodiesnest@gmail.com</p>
+            <p>+91 9898989898</p>
+            <p>29 Food Street </p>
+        </div>
+        <div className="footer-section">
+            <p className="title">Social Platform</p>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+        </div>
+    </div>
+    )
+}
